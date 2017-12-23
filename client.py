b5157a5135a65ed04211af769c00464f67f81fc6
Trevor Nelson <teafiend9@gmail.com>
2017-12-23 10:53:58 -0800

    Add threaded broadcast sockets to allow for real-time UI updates

--- client.py
+++ client.py
@@ -1,18 +1,20 @@
 import socket
 import time
+import threading
 
 PORT = 10101
 HOST = "localhost"
 
 def process_user_input(client_socket, username):
+    # Show user interface
+    users, messages = get_interface_data(client_socket)
+    display_interface(users, messages)
+
     user_connected = True
 
     while user_connected:
-        # Show user interface
-        users, messages = get_interface_data(client_socket)
-        display_interface(users, messages)
     
-        user_msg = input("Enter message (or QUIT): ")
+        user_msg = input()
         if user_msg.lower() == "quit":
             user_connected = False
         else:
@@ -38,7 +40,9 @@ def get_interface_data(client_socket):
 
     return users, messages
 
+
 def display_interface(users, messages):
+    print("\n" * 100)
     print("\n### Users ###")
     for user in users:
         print(user)
@@ -46,6 +50,30 @@ def display_interface(users, messages):
     print("\n### Messages ###")
     for msg in messages:
           print("{}: {}".format(msg[0], msg[1]))
+
+    print("\nEnter a message (or QUIT): ")
+
+
+def create_listener(host, port):
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.connect((host, port))
+    listen_socket.send(b"listen\r\n")
+    listen_socket.recv(1024)
+    listen_thread = threading.Thread(target=listen, args=(listen_socket,))
+    listen_thread.start()
+
+    
+def listen(listen_socket):
+    while True:
+        msg = listen_socket.recv(1024).decode().rstrip("\r\n")
+        msg_lines = msg.split("\r\n")
+        users = msg_lines[0].split(":")[1].split(",")
+        messages = []
+        for line in msg_lines[2:]:
+            tokens = line.split("::")
+            messages.append((tokens[0], tokens[1]))
+        display_interface(users, messages)
+            
 
 if __name__ == "__main__":
     print("Connecting to server...")
@@ -58,5 +86,7 @@ if __name__ == "__main__":
     username = input("Hello, please enter a username: ")
     client_socket.send("hello {}\r\n".format(username).encode())
     client_socket.recv(1024)
+
+    create_listener(HOST, PORT)
 
     process_user_input(client_socket, username)
--- server.py
+++ server.py
@@ -22,50 +22,96 @@ import threading
 PORT = 10101
 message_log = []
 users = []
-new_message = False
+broadcast_sockets = set()
+
+
+def send_broadcast():
+    """
+    Format:
+        users:<user1>,<user2>,<user3>\r\n
+        messages:\r\n
+        <user1>::<message1>\r\n
+        <user2>::<message2>\r\n
+    """
+    msg_lines = []
+    msg_lines.append("users:" + users_as_str() + "\r\n")
+    msg_lines.append("messages:\r\n")
+
+    for line in messages_as_str():
+        msg_lines.append(line)
+
+    msg_str = "".join(msg_lines)
+
+    for listener in broadcast_sockets:
+        listener.send(msg_str.encode())
+
+
+def messages_as_str():
+    msg_list = []
+
+    for msg in message_log:
+        msg_as_str = "{}::{}".format(msg[0], msg[1])
+        msg_list.append(msg_as_str)
+
+    return "\r\n".join(msg_list)
+    
+def users_as_str():
+    return ",".join(users)
+
+
+def send_message_log(connection_socket):
+    msgs_as_str = messages_as_str() + "\r\n"
+    connection_socket.send(msgs_as_str.encode())
+
+    
+def send_users(connection_socket):
+    users_str = users_as_str() + "\r\n"
+    connection_socket.send(users_str.encode())
 
 
 def process_client_messages(connection_socket, username):
     connection_open = True
     while connection_open:
-        
-        message = connection_socket.recv(1024).decode().rstrip("\r\n")
     
+        message = connection_socket.recv(1024).decode().rstrip("\r\n")
+        
         if message == "get messages":
-            msg_list = []
-            for msg in message_log:
-                msg_as_str = "{}::{}\r\n".format(msg[0], msg[1])
-                msg_list.append(msg_as_str)
-            msgs_as_str = "".join(msg_list) + "\r\n"
-            connection_socket.send(msgs_as_str.encode())
+                send_message_log(connection_socket)
 
         if message == "get users":
-            users_as_str = ",".join(users) + "\r\n" # consider storing users as list instead
-            connection_socket.send(users_as_str.encode())
+                send_users(connection_socket)
 
         if message[0:5] == "send ":
             user_msg = message[5:]
             message_log.append((username, user_msg))
             connection_socket.send(b"message received\r\n")
-                
+            send_broadcast()
+            
         elif message == "quit":
             connection_socket.send(b"goodbye\r\n")
             connection_open = False
 
     connection_socket.close()
 
-        
+    
 def handle_connection(connection_socket):
     # Handshake
-    message = connection_socket.recv(1024).decode()
-    username = message.rstrip("\r\n")[6:] # Exclude "hello" of message: "hello <username>"
-    connection_socket.send("welcome {}\r\n".format(username).encode())
+    message = connection_socket.recv(1024).decode().rstrip("\r\n")
+    prefix  = message.split()[0]
 
-    if username not in users:
-        users.append(username)
+    if prefix == "hello":
+        username = message[6:] # Exclude "hello" of message: "hello <username>"
+        connection_socket.send("welcome {}\r\n".format(username).encode())
 
-    process_client_messages(connection_socket, username)
-    
+        if username not in users:
+            users.append(username) # TODO: Maybe use a set for users instead?
+
+        process_client_messages(connection_socket, username)
+
+    elif prefix == "listen":
+        broadcast_sockets.add(connection_socket)
+        connection_socket.send(b"listener added\r\n")
+
 
 if __name__ == "__main__":
     # Setup TCP server
